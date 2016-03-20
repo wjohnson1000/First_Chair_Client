ed03bcc91f5167b519e9deb56397caa35d4e5b16
William Johnson <wjohnson1000@gmail.com>
2016-03-20 12:03:48 -0600

    get request to auth route

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,12 @@
 app.controller('landing', ['$scope', function($scope){
   console.log('hello from controllers');
   $scope.login = function(){
-    
+    $http.get("https://firstchair.herokuapp.com")
+      .then(function(response){
+        console.log(response)
+      })
+      .catch(function(error){
+        console.error('could not log in')
+      })
   }
 }]);
